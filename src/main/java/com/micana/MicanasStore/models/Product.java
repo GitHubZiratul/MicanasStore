11264ce9063be0f6adbf628d677181e2b996113e
Micana <islam.kurbanov.96.96@mail.ru>
2023-01-07 04:08:03 +0400

    Fix bugs, fixed user authorization Added OneToMany relation between User and Product

--- src/main/java/com/micana/MicanasStore/models/Product.java
+++ src/main/java/com/micana/MicanasStore/models/Product.java
@@ -26,11 +26,13 @@ public class Product {
     private int price;
     @Column(name = "genre")
     private String genre;
-    @Column(name = "author")
-    private String author;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
             mappedBy = "product")
     private List<Image> images = new ArrayList<>();
+    @ManyToOne(cascade = CascadeType.REFRESH,fetch = FetchType.LAZY)
+    @JoinColumn
+    private User user;
     private Long previewImageId;
     private LocalDateTime dateOfCreated;
 
